66ba3d447a6cf781dd00d979d3b657d85253caea
user <andervol2015@gmail.com>
2025-02-12 11:25:59 +0300

    HW-7

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,86 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Задание 1");
+        int salary = 0;
+        int month = 1;
+        do {
+            salary = salary + 15000;
+            System.out.println("Месяц " + month + ", сумма накоплений равна " + salary + " рублей");
+            month++;
+        }
+        while (salary <= 2_459_000);
+        System.out.println();
+        System.out.println("Задание 2");
+        int i = 1;
+        do {
+            System.out.print(" " + i);
+            i++;
+        }
+        while (i <= 10);
+        System.out.println();
+        for (i = 10; i >= 1; i--) {
+            System.out.print(" " + i);
+        }
+        System.out.println();
+        System.out.println("Задание 3");
+        int population = 12_000_000;
+        int year = 1;
+        do {
+            population = population + population / 1000 * 9;
+            System.out.println("Год " + year + ", численность населения составляет " + population);
+            year++;
+        }
+        while (year <= 10);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println();
+        System.out.println("Задание 4");
+        int deposit = 15000;
+        int montH = 1;
+        do {
+            deposit = deposit + deposit / 100 * 7;
+            System.out.println("Месяц " + montH + ", сумма накоплений составляет " + deposit);
+            montH++;
+        }
+        while (deposit <= 12_000_000);
+        System.out.println();
+        System.out.println("Задание 5");
+        int deposiT = 15000;
+        int monTH = 0;
+        do {
+            deposiT = deposiT + deposiT / 100 * 7;
+            monTH++;
+            if (monTH % 6 == 0) {
+                System.out.println("Месяц " + monTH + ", сумма накоплений составляет " + deposiT);
+            }
+        }
+        while (deposiT <= 12_000_000);
+        System.out.println();
+        System.out.println("Задание 6");
+        int deposIT = 15000;
+        int moNTH = 0;
+        do {
+            deposIT = deposIT + deposIT / 100 * 7;
+            moNTH++;
+            if (moNTH % 6 == 0) {
+                System.out.println("Месяц " + moNTH + ", сумма накоплений составляет " + deposIT);
+            }
+        }
+        while (moNTH <= 108);
+
+        System.out.println();
+        System.out.println("Задание 7");
+        int friday = 0;
+        for (i = 0; i <= 31; i = i + 1) {
+            if (i % 7 == 0 && i != 0) {
+                System.out.println("Сегодня пятница, " + i + "-е число ");
+                friday = i;
+            }
+        }
+        System.out.println();
+        System.out.println("Задание 8");
+        for (i = 0; i <= 2125; i = i + 1) {
+            if (i % 79 == 0 && i >= 1825)
+                System.out.println(i);
         }
     }
 }
